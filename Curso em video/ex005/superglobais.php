c26260eba9ef7834d851ad2298bd41852abc1613
Jonatas Oliveira <a42688454@gmail.com>
2024-03-02 12:20:49 -0300

    last

---
+++ Curso em video/ex005/superglobais.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super globais</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <section>
+        <?php 
+            var_dump($_POST)
+        ?>
+    </section>
+</body>
+</html>
